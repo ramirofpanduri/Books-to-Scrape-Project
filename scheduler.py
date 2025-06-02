63f0d9daafc21133a780424b023a8dec8f052d46
Ramiro <ramirofpanduri@gmail.com>
2025-06-02 18:07:32 -0300

    Scheduler added

---
+++ scheduler.py
@@ -0,0 +1,14 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+import subprocess
+
+
+def run_main():
+    subprocess.run(["python", "main.py"], check=True)
+
+
+scheduler = BlockingScheduler()
+scheduler.add_job(run_main, 'interval', hours=10)
+
+if __name__ == "__main__":
+    print("Scheduler started. It will run every 10 hours.")
+    scheduler.start()
